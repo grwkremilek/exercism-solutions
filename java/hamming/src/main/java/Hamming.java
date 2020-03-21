9b279d9e9445815b3c4d1ab30a50de93a4add20c
grw.kremilek <zuzana.krenkova@gmail.com>
2020-03-21 23:57:15 +0100

    IllegalArgumentException

---
+++ java/hamming/src/main/java/Hamming.java
@@ -0,0 +1,21 @@
+class Hamming {
+    
+    private String left;
+    private String right;
+
+    Hamming(String leftStrand, String rightStrand){
+        if (leftStrand.length() != rightStrand.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+        left = leftStrand;
+        right = rightStrand;
+    }
+
+    int getHammingDistance() {
+        int count = 0;
+        for (int i = 0; i < left.length(); ++i){
+            if (left.charAt(i) != right.charAt(i)) {count += 1;}
+        }
+        return count;
+    }
+}
